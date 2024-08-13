b97fd49f63107bc3813067cd41665b23df7538b8
LeslieAmaya <110561410+LeslieAmaya@users.noreply.github.com>
2024-08-13 11:37:07 -0700

    infected eliminado

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,6 @@
 const { Router } = require("express");
 const login = require("../controllers/login.controller");
 const userController = require("../controllers/user.controller");
-const infectedController = require("../controllers/infected.controller");
-const stateController = require("../controllers/state.controller");
-const campainController = require("../controllers/campain.controller");
 
 const router = Router();
 //http://localhost/api/login - post {username: "Isael", password: "Isael123" }
@@ -15,22 +12,5 @@ router.get("/user", userController.getUser);
 router.put("/user/:id", userController.updateUser);
 // get obtener los datos de la base de datos
 // post enviar los datos a la base de datos
-
-// Rutas catalogo de infectados
-router.post("/infected", infectedController.createInfected);
-router.delete("/infected/:id", infectedController.deleteInfected);
-router.get("/infected", infectedController.getAllInfected);
-router.get("/infected/:id", infectedController.getInfectedById);
-router.put("/infected/:id", infectedController.updateInfected);
-
-// Rutas de estados
-router.get("/state", stateController.getAllStates);
-
-// Rutas catalogo de campañas
-router.post("/campain", campainController.createCampain);
-router.delete("/campain/:id", campainController.deleteCampain);
-router.put("/campain/:id", campainController.updateCampain);
-router.get("/campain", campainController.getAllCampain);
-router.get("/campain/:id", campainController.getCampainById);
 
 module.exports = router;
