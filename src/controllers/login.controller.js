c371fa7cdd10424155cad272ede14af8966c768d
LeslieAmaya <leslieamayal001@gmail.com>
2024-08-20 20:33:16 -0700

    rol en send

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,14 +1,14 @@
 const  User = require('../models/user.model');
 
 exports.login = async (req, res) => { //Espera peticion y devuelve respuesta
-    const {username, password } = req.body;
+    const {username, password, rol } = req.body;
     console.log(req.body);
 
     try{
         const user = await User.findOne({username});
         /* creaUsuario(); */
         if (user && (password== user.password)){
-            res.send({ username, password});
+            res.send({ username, password, rol});
             console.log({user});
         } else {
             res.status(501).json({message: "Usuario o contraseña incorrecta"})
